675a648dd0ff011940ee422e2dbce6564474a4fe
Lucas-Rubia <lucas.de.oliveira.rubia@gmail.com>
2024-11-18 23:46:15 -0300

    feat: Add Header and first section home page

--- src/components/header/menu-links.tsx
+++ src/components/header/menu-links.tsx
@@ -1,6 +1,6 @@
 import { ChevronRight } from "lucide-react";
 import { Button } from "../ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 export function MenuLinks() {
   return (
